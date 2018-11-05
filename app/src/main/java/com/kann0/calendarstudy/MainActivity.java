14d6b80c85419dd8c3d89c1403794e3bd9d8f618
m-kann0 <m.kanno.0915@gmail.com>
2018-11-06 00:35:03 +0900

    Implement read and logging

--- app/src/main/java/com/kann0/calendarstudy/MainActivity.java
+++ app/src/main/java/com/kann0/calendarstudy/MainActivity.java
@@ -1,13 +1,80 @@
 package com.kann0.calendarstudy;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private static final String[] EVENT_PROJECTION = new String[]{
+            CalendarContract.Calendars._ID,
+            CalendarContract.Calendars.ACCOUNT_NAME,
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+            CalendarContract.Calendars.OWNER_ACCOUNT,
+    };
+
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readCalendars();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            readCalendars();
+        } else {
+            Log.d(TAG, "onRequestPermissionsResult: permission denied...");
+        }
+    }
+
+    private void readCalendars() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CALENDAR},
+                    PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+        try (Cursor cursor = contentResolver.query(
+                CalendarContract.Calendars.CONTENT_URI, EVENT_PROJECTION, null, null, null)) {
+            if (cursor == null) {
+                return;
+            }
+            while (cursor.moveToNext()) {
+                long calId = cursor.getLong(PROJECTION_ID_INDEX);
+                String displayName = cursor.getString(PROJECTION_DISPLAY_NAME_INDEX);
+                String accountName = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+                String ownerName = cursor.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+                Log.d(TAG, "onCreate: calId=" + calId
+                        + ", displayName=" + displayName
+                        + ", accountName=" + accountName
+                        + ", ownerName=" + ownerName);
+            }
+        }
     }
 }
