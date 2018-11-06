db372274467c47ab0f1a63ffe6dc65b5a52cbcd5
m-kann0 <m.kanno.0915@gmail.com>
2018-11-06 23:05:47 +0900

    Add job skelton

---
+++ app/src/main/java/com/kann0/calendarstudy/CalendarContentJob.java
@@ -0,0 +1,7 @@
+package com.kann0.calendarstudy;
+
+public class CalendarContentJob {
+
+    // TODO implement this class with reference to the following sample
+    // https://developer.android.com/reference/android/app/job/JobInfo.Builder#addTriggerContentUri(android.app.job.JobInfo.TriggerContentUri)
+}
